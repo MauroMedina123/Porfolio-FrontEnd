944fbb188054a4314b4b57baed45cb70b52189b2
Medina Mauro <M.Xtreme123@Gmail.com>
2022-08-29 20:11:45 -0300

    Primer commit: Carpeta Components y Servicios creadas; librerias bootstrap, jquery y popper agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/Header/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/Header/banner/banner.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EstudiosComponent,
+    ExperienciaLaboralComponent,
+    HardAndSoftSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
